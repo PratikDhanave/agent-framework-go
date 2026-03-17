d3f225d6f8a81c8336db9d9b302309f8ff5bdf89
Quim Muntal <quimmuntal@gmail.com>
2026-03-17 09:52:44 +0100

    Implement A2A hosting (#90)
    * implement A2A hosting
    
    * codereview comments
    
    * simplify a2ahosting api
    
    * don't use mapsOrNil

---
+++ hosting/a2ahosting/a2a.go
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+
+package a2ahosting
+
+import (
+	"net/http"
+
+	"github.com/a2aproject/a2a-go/a2asrv"
+)
+
+func NewRequestHandler(cfg ExecutorConfig, options ...a2asrv.RequestHandlerOption) a2asrv.RequestHandler {
+	if cfg.Agent == nil {
+		panic("agent is required")
+	}
+	return a2asrv.NewHandler(NewExecutor(cfg), options...)
+}
+
+func NewHTTPHandler(cfg ExecutorConfig, options ...a2asrv.RequestHandlerOption) http.Handler {
+	return a2asrv.NewJSONRPCHandler(NewRequestHandler(cfg, options...))
+}
